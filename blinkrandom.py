a49f919fb968fe01bbd511f912f0303fbfa71eaa
Matt <phatcatmatt@gmail.com>
2017-04-12 21:04:06 -0600

    removed logs, fixed bugs

--- blinkrandom.py
+++ blinkrandom.py
@@ -1,35 +1,31 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 import sys
 import random
-
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(pin1, GPIO.OUT)
-# GPIO.setup(pin2, GPIO.OUT)
 
 pin1 = int(sys.argv[1])
 pin2 = int(sys.argv[2])
 pin1Val = False
 pin2Val = False
 
-print "blinking on", pin1, pin2
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin1, GPIO.OUT)
+GPIO.setup(pin2, GPIO.OUT)
+
+print("blinking on", pin1, pin2)
 
 while True:
     pin = random.randint(pin1,pin2)
     if pin == pin1:
         pin1Val = not pin1Val
         if pin1Val:
-            print(pin1, pin1Val)
-            # GPIO.output(pin1, GPIO.HIGH)
+            GPIO.output(pin1, GPIO.HIGH)
         else:
-            print(pin1,pin1Val)
-            # GPIO.output(pin1, GPIO.LOW)
+            GPIO.output(pin1, GPIO.LOW)
     elif pin == pin2:
         pin2Val = not pin2Val
         if pin2Val:
-            print(pin2, pin2Val)
-            # GPIO.output(pin2, GPIO.HIGH)
+            GPIO.output(pin2, GPIO.HIGH)
         else:
-            print(pin2, pin2Val)
-            # GPIO.output(pin2, GPIO.LOW)
+            GPIO.output(pin2, GPIO.LOW)
     time.sleep(random.random())
