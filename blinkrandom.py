1b2cceaa799522dec5d80d472d871bec44e05d9a
Matt <phatcatmatt@gmail.com>
2017-04-13 21:58:52 -0600

    replaced nest if with pythony ternary operator type thing

--- blinkrandom.py
+++ blinkrandom.py
@@ -18,14 +18,8 @@ while True:
     pin = random.randint(pin1,pin2)
     if pin == pin1:
         pin1Val = not pin1Val
-        if pin1Val:
-            GPIO.output(pin1, GPIO.HIGH)
-        else:
-            GPIO.output(pin1, GPIO.LOW)
+        GPIO.output(pin1, GPIO.HIGH if pin1Val else GPIO.LOW)
     elif pin == pin2:
         pin2Val = not pin2Val
-        if pin2Val:
-            GPIO.output(pin2, GPIO.HIGH)
-        else:
-            GPIO.output(pin2, GPIO.LOW)
+        GPIO.output(pin2, GPIO.HIGH if pin2Val else GPIO.LOW)
     time.sleep(random.uniform(0, .7))
